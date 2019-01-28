e5c922584d26031880350c231c4f49b6dfd49c42
Craig <ccovell@hotmail.com>
2019-01-28 12:02:24 -0500

    Task_List3/Tasks_3_Answers.py

---
+++ Task_List_3/Tasks_3_Answers.py
@@ -0,0 +1,21 @@
+# Answers for Tasks in Module Five
+
+
+# Task 1 
+# Write a loop that accumulates the sum of all the numbers in a list (given below) and prints the sum in the end.
+#
+# Data = [0, -1, 2, -3, 4, -5, 6, -7, 8]
+#
+
+# Task 2
+# Write a loop that accumulates the sum of absolute value of all the numbers in a list (given below) and prints the sum in the end.
+#
+# Data = [0, -1, 2, -3, 4, -5, 6, -7, 8]
+#
+
+# Please upload the Python code as a single .py file
+
+
+
+# Task_1
+Data = [0, -1, 2, -3, 4, -5, 6, -7, 8]
